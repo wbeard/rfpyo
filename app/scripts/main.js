e7bae8ac39d2d46484dddf3fe4cb467d63e4faa6
William Beard <disenno@gmail.com>
2014-03-31 22:37:31 -0500

    Added remove

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
 var app = angular.module('app', []);
 
 app.controller('AppController', function($scope) {
@@ -10,9 +16,19 @@ app.controller('AppController', function($scope) {
       "tags": tags
     });
     localStorage.setItem("phrases", angular.toJson($scope.phrases));
+  };
+
+  var _removePhrase = function(index) {
+    $scope.phrases.remove(index);
+    localStorage.setItem("phrases", angular.toJson($scope.phrases));
   }
 
   $scope.addPhrase = function() {
     _addPhrase($("#newPhraseText").val(), $("#newPhraseTags").val().split(", "))
   };
+
+  $scope.removePhrase = function(index) {
+    _removePhrase(index);
+  };
+
 });
